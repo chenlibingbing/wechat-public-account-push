ad7649d30832552cbbe65e2fba2c6af1e1def344
chenlibingbing <115129003+chenlibingbing@users.noreply.github.com>
2022-10-07 10:20:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '哥哥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oN6h75lY05nplM5_Er28mGb0JwkM',
+      id: 'o1clv52Uxp7_6c5p-wVevvqyc1nY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oN6h75vRlKeQPU8QLsYEap6epuxA',
+      useTemplateId: 'tN--1gFsV3baDVeNu5wsETcL3iQDA-uEi153_PIXYb4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-21',
       festivals: [
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oN6h75lY05nplM5_Er28mGb0JwkM',
+      id: 'o1clv558hceFnadwNE3mvdLiumVA',
     }
   ],
 
